9437812a500c2726a42de906a5f897ed369d3579
Olga Korol <oggi.fine@gmail.com>
2023-11-22 17:28:05 +0300

    initial commit

---
+++ src/test/java/CardWithDeliveryTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+public class CardWithDeliveryTest {
+
+    private Faker faker;
+
+    @BeforeEach
+    void setUpAll() {
+        faker= new Faker(new Locale("ru"));
+    }
+
+    @Test
+    void  should() {
+        String
+    }
+}
